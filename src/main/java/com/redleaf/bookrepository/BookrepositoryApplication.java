8e10b82c662b7b50034934b0b3bf695041d0d159
angusbagchee <spare_brain@yahoo.com>
2018-04-01 17:59:57 -0400

    initial commit

---
+++ src/main/java/com/redleaf/bookrepository/BookrepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.redleaf.bookrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrepositoryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/redleaf/bookrepository/BookrepositoryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.redleaf.bookrepository;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookrepositoryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
